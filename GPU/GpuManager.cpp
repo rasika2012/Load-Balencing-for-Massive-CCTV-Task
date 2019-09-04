acaa48db5e72bfc6f368acda351ff029971c2ac9
chathuranga <chathurangap688@gmail.com>
2019-09-05 01:23:42 +0530

    take system envirement

---
+++ GPU/GpuManager.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+#include <string> 
+class GpuManager
+{
+private:
+    /* data */
+public:
+    string GetStdoutFromCommand(string cmd) {
+
+        string data;
+        FILE * stream;
+        const int max_buffer = 256;
+        char buffer[max_buffer];
+        cmd.append(" 2>&1");
+
+        stream = popen(cmd.c_str(), "r");
+        if (stream) {
+            while (!feof(stream))
+                if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
+                    pclose(stream);
+        }
+
+        return data;
+    }
+
+};
+
--- main.cpp
+++ main.cpp
@@ -1,36 +1,22 @@
 #include <iostream> 
 #include <string> 
 #include "FeedManager.cpp"
+#include "GPU/GpuManager.cpp"
 using namespace std; 
-
-string GetStdoutFromCommand(string cmd) {
-
-    string data;
-    FILE * stream;
-    const int max_buffer = 256;
-    char buffer[max_buffer];
-    cmd.append(" 2>&1");
-
-    stream = popen(cmd.c_str(), "r");
-    if (stream) {
-        while (!feof(stream))
-            if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
-                pclose(stream);
-    }
-
-    return data;
-}
 
 int main(int argc, char const *argv[])
 {
-    int a = 0;
+    int gpuCount = 0;
+    GpuManager gpuManager;
     FeedManager feedManager;
     // Thread 1
     feedManager.server();
     // Thread 2
     feedManager.client();
-    a = stoi(GetStdoutFromCommand("nproc"));
-    printf("%d\n",a);
+
+
+    gpuCount = stoi(gpuManager.GetStdoutFromCommand("nproc"));
+    printf("%d\n",gpuCount);
 
     return 0;
 }
