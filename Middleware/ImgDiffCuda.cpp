d6f1d1a6b503158550e11d53b549bc94b56b5f44
chathuranga <chathurangap688@gmail.com>
2019-10-14 23:08:15 +0530

    change class

--- Middleware/ImgDiffCuda.cpp
+++ Middleware/ImgDiffCuda.cpp
@@ -13,17 +13,17 @@ struct BufferPSNR                                     // Optimized CUDA versions
 
     cuda::GpuMat buf;
 };
-public double getPSNR_CUDA_optimized(const Mat& I1, const Mat& I2, BufferPSNR& b)
+int getPSNR_CUDA_optimized(const Mat& I1, const Mat& I2, BufferPSNR& b)
 {
     b.gI1.upload(I1);
     b.gI2.upload(I2);
-
+    int val = 0;
     b.gI1.convertTo(b.t1, CV_32F);
     b.gI2.convertTo(b.t2, CV_32F);
 
     cuda::absdiff(b.t1.reshape(1), b.t2.reshape(1), b.gs);
     cuda::multiply(b.gs, b.gs, b.gs);
-    // imshow( "privius", b.gs );
+    // imshow( "privius", I1 );
     double sse = cuda::sum(b.gs, b.buf)[0];
 
     if( sse <= 1e-10) // for small values return zero
@@ -31,7 +31,9 @@ public double getPSNR_CUDA_optimized(const Mat& I1, const Mat& I2, BufferPSNR& b
     else
     {
         double mse = sse /(double)(I1.channels() * I1.total());
-        double psnr = 10.0*log10((255*255)/mse);
-        return psnr;
+        double psnr = log10((255*255)/mse);
+        val = round(psnr);
+        // printf("%d\n",val);
+        return val;
     }
 }
--- Middleware/middlewareoop.cpp
+++ Middleware/middlewareoop.cpp
@@ -13,6 +13,7 @@
 #include <sstream>
 #include <unistd.h>
 #include <queue>
+#include "ImgDiffCuda.cpp"
 
 void *display(void *);
 char **args;
@@ -27,36 +28,13 @@ queue <Mat> to_send_data;
     int rmport,srvport;
 //
 
-struct BufferPSNR                                     // Optimized CUDA versions
-{   // Data allocations are very expensive on CUDA. Use a buffer to solve: allocate once reuse later.
-    cuda::GpuMat gI1, gI2, gs, t1,t2;
+// struct BufferPSNR                                     // Optimized CUDA versions
+// {   // Data allocations are very expensive on CUDA. Use a buffer to solve: allocate once reuse later.
+//     cuda::GpuMat gI1, gI2, gs, t1,t2;
 
-    cuda::GpuMat buf;
-};
-int getPSNR_CUDA_optimized(const Mat& I1, const Mat& I2, BufferPSNR& b)
-{
-    b.gI1.upload(I1);
-    b.gI2.upload(I2);
-    int val = 0;
-    b.gI1.convertTo(b.t1, CV_32F);
-    b.gI2.convertTo(b.t2, CV_32F);
+//     cuda::GpuMat buf;
+// };
 
-    cuda::absdiff(b.t1.reshape(1), b.t2.reshape(1), b.gs);
-    cuda::multiply(b.gs, b.gs, b.gs);
-    // imshow( "privius", I1 );
-    double sse = cuda::sum(b.gs, b.buf)[0];
-
-    if( sse <= 1e-10) // for small values return zero
-        return 0;
-    else
-    {
-        double mse = sse /(double)(I1.channels() * I1.total());
-        double psnr = log10((255*255)/mse);
-        val = round(psnr);
-        // printf("%d\n",val);
-        return val;
-    }
-}
 void clean_q_r(){
     if(resived_data.size() > 150 )
     while (resived_data.size() > 100 ){
