88bf7e92538918fa7572f1cff512780ce236580a
RochaaP <rochpathiraja@gmail.com>
2020-05-24 16:45:07 +0530

    ui to view cameras

--- Cleaned/viewer/pager/src/App.js
+++ Cleaned/viewer/pager/src/App.js
@@ -3,12 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import {Timing} from './Component/timing'
 import Home from './Component/Home';
+import Add from './Component/Camera/Add'
+
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'; 
+// import Link from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-        <Home/>
-    </div>
+
+    <BrowserRouter>
+      <Switch>
+        <Route path="/"  exact component={Home} />
+        <Route path="/add" exact component={Add} />
+      </Switch>
+    </BrowserRouter>
+    
   );
 }
 
